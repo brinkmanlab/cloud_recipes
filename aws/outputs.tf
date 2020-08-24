613b4df059d47268c7e70a332898a7b03d57be04
Nolan Woods <nolan_w@sfu.ca>
2020-08-24 14:52:55 -0700

    output entire submodules

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,23 +1,7 @@
-output "cluster_id" {
-  value = module.eks.cluster_id
+output "eks" {
+  value = module.eks
 }
 
-output "worker_security_group_id" {
-  value = module.eks.worker_security_group_id
-}
-
-output "database_subnet_group" {
-  value = module.vpc.database_subnet_group
-}
-
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "cdir_block" {
-  value = module.vpc.vpc_cidr_block
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
+output "vpc" {
+  value = module.vpc
 }
