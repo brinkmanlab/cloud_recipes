5dd0d5363386de5e3d76c966399da3fb5f2b7f9d
Nolan Woods <nolan_w@sfu.ca>
2020-08-25 15:55:28 -0700

    Disable writing kubeconfig

--- aws/eks.tf
+++ aws/eks.tf
@@ -15,11 +15,12 @@ locals {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.17"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  source           = "terraform-aws-modules/eks/aws"
+  cluster_name     = var.cluster_name
+  cluster_version  = "1.17"
+  subnets          = module.vpc.private_subnets
+  vpc_id           = module.vpc.vpc_id
+  write_kubeconfig = false
 
   manage_aws_auth               = true
   cluster_create_security_group = true
