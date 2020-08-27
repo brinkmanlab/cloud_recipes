ed54be99265f8757d566eac6e807accca50dd813
Nolan Woods <nolan_w@sfu.ca>
2020-08-26 17:48:02 -0700

    Add TODO

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,6 @@
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
+# TODO https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html
+# Allow users with Admin role
