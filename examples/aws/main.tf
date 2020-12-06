5a5f372e21341d0f6a54a03adebe17553e07da3c
Nolan Woods <nolan_w@sfu.ca>
2020-12-06 13:37:12 -0800

    add test pvc and job

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -23,7 +23,60 @@ provider "kubernetes" {
 }
 
 module "cvmfs" {
-  source     = "../../util/k8s/cvmfs"
-  cvmfs_keys = { "microbedb.brinkmanlab.ca" : file("microbedb.brinkmanlab.ca.pub") }
-  servers    = ["http://stratum-0.brinkmanlab.ca/cvmfs/@fqrn@"]
+  source = "../../util/k8s/cvmfs"
+  cvmfs_keys = {
+    "microbedb.brinkmanlab.ca" = file("microbedb.brinkmanlab.ca.pub")
+  }
+  servers = ["http://stratum-0.brinkmanlab.ca/cvmfs/@fqrn@"]
+}
+
+resource "kubernetes_persistent_volume_claim" "repo" {
+  wait_until_bound = false
+  metadata {
+    name      = "microbedb.brinkmanlab.ca"
+    namespace = "cvmfs"
+  }
+  spec {
+    access_modes       = ["ReadOnlyMany"]
+    storage_class_name = module.cvmfs.storageclasses["microbedb.brinkmanlab.ca"].metadata.0.name
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_job" "test" {
+  metadata {
+    name      = "test"
+    namespace = "cvmfs"
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        container {
+          name    = "test"
+          image   = "alpine"
+          command = ["ls", "-l", "/cvmfs"]
+          volume_mount {
+            mount_path = "/cvmfs"
+            name       = "cvmfs"
+            read_only  = true
+          }
+        }
+        node_selector = {
+          WorkClass = "service"
+        }
+        volume {
+          name = "cvmfs"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.repo.metadata.0.name
+          }
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
 }
