9f02f946a9d12b1d67050f53f12d3cadf698a7d3
Nolan Woods <nolan_w@sfu.ca>
2022-01-14 11:38:36 -0800

    Reevaluate swarm tokens when re-initialising swarm

--- openstack/swarm/main.tf
+++ openstack/swarm/main.tf
@@ -44,7 +44,7 @@ resource "openstack_images_image_v2" "engine" {
 resource "sshcommand_command" "manager_token" {
   depends_on  = [sshcommand_command.init_swarm]
   host        = openstack_compute_floatingip_associate_v2.manager1.floating_ip
-  command     = "docker swarm join-token -q manager"
+  command     = "docker swarm join-token -q manager #${sshcommand_command.init_swarm.id}"
   private_key = var.private_key
   user        = var.vm_user
 }
@@ -52,7 +52,7 @@ resource "sshcommand_command" "manager_token" {
 resource "sshcommand_command" "worker_token" {
   depends_on  = [sshcommand_command.init_swarm]
   host        = openstack_compute_floatingip_associate_v2.manager1.floating_ip
-  command     = "docker swarm join-token -q worker"
+  command     = "docker swarm join-token -q worker #${sshcommand_command.init_swarm.id}"
   private_key = var.private_key
   user        = var.vm_user
 }
