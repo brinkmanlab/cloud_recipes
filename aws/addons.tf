6f7b49f3856a958e148c45072656004d718cd2c4
cookersjs <cookersjs@gmail.com>
2025-10-01 16:43:43 -0700

    Remove depends_on and iam_service_account_role_arn calls

--- aws/addons.tf
+++ aws/addons.tf
@@ -1,10 +1,12 @@
 # Data sources to get cluster info
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 # Kubernetes provider — use alias to prevent conflicts
@@ -13,8 +15,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-
-  depends_on = [module.eks] # wait until cluster exists
 }
 
 # Optional: Wait for at least 1 node to be ready
@@ -22,7 +22,7 @@ resource "null_resource" "wait_for_nodes" {
   provisioner "local-exec" {
     command = "kubectl --kubeconfig <(aws eks update-kubeconfig --name ${module.eks.cluster_name}) wait --for=condition=Ready nodes --all --timeout=600s"
   }
-  depends_on = [module.eks]
+  
 }
 
 # ALB Ingress Controller
@@ -38,8 +38,4 @@ module "alb_ingress_controller" {
   k8s_namespace    = "kube-system"
   aws_region_name  = data.aws_region.current.name
   k8s_cluster_name = module.eks.cluster_name
-
-  iam_service_account_role_arn = module.eks.iam_oidc_provider_arn
-
-  depends_on = [module.eks.cluster_name, null_resource.wait_for_nodes]
 }
