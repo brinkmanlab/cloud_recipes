64bded26216f8f184f4e1743483f565e71c8d669
cookersjs <cookersjs@gmail.com>
2025-10-05 20:57:42 -0700

    Revert eks.tf

--- aws/eks.tf
+++ aws/eks.tf
@@ -86,19 +86,19 @@ module "eks" {
 
 }
 
-#data "aws_eks_cluster" "cluster" {
-#  name = module.eks.cluster_name
-#}
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
 
-#data "aws_eks_cluster_auth" "cluster" {
-#  name = module.eks.cluster_name
-#}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
 
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.cluster.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
 
 # IAM role for EKS Node Groups
@@ -160,5 +160,5 @@ module "alb_ingress_controller" {
   k8s_namespace    = "kube-system"
 
   aws_region_name  = data.aws_region.current.name
-  k8s_cluster_name = module.eks.cluster_name
+  k8s_cluster_name = data.aws_eks_cluster.cluster.name
 }
